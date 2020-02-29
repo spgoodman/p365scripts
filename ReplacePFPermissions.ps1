e7a6043caabb145147ab663eee69db1cd665b9a9
Steve Goodman <steve@goodman.net>
2020-02-29 12:22:45 +0000

    Update ReplacePFPermissions.ps1

--- ReplacePFPermissions.ps1
+++ ReplacePFPermissions.ps1
@@ -1,12 +1,16 @@
-param($NewUser,$FolderTree,$AccessRights,$TestMode=$False)
+param($NewUser,$FolderTree,$AccessRights,$TestMode=$False,$KeepDefaultAnon=$True)
 [array]$Folders = Get-PublicFolder -Identity $FolderTree -Recurse -ResultSize Unlimited -ErrorAction SilentlyContinue
 if (!$Folders) { throw "Folder $($FolderTree) not found" }
 $Recipient = Get-Recipient -Identity $NewUser -ErrorAction SilentlyContinue
 if (!$Recipient) {throw "Recipient $($NewUser) not found" }
 foreach ($Folder in $Folders)
 {
-    [array]$ExistingPermissions = Get-PublicFolderClientPermission -Identity $Folder | Where {$_.User -notlike "Default" -and $_.User -notlike "Anonymous"}
-    
+    if ($KeepDefaultAnon)
+    {
+        [array]$ExistingPermissions = Get-PublicFolderClientPermission -Identity $Folder | Where {$_.User -notlike "Default" -and $_.User -notlike "Anonymous"}
+    } else {
+        [array]$ExistingPermissions = Get-PublicFolderClientPermission -Identity $Folder
+    }
     if ($TestMode)
     {
             
