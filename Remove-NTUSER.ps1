6c370691a4ce594fc20df9360bd39b702ee597eb
Steve Goodman <steve@goodman.net>
2020-03-01 21:44:18 +0000

    Add files via upload

---
+++ Remove-NTUSER.ps1
@@ -0,0 +1,32 @@
+﻿param($Remove=$False,$TestMode=$False)
+[array]$Folders = Get-PublicFolder -Identity "\" -Recurse -ResultSize Unlimited
+foreach ($Folder in $Folders)
+{
+    [array]$FolderPermissions = Get-PublicFolderClientPermission -Identity $Folder.Identity | Where {$_.User -like "NT User:*"}
+    if ($FolderPermissions)
+    {
+        foreach ($FolderPermission in $FolderPermissions)
+        {
+            Write-Host -NoNewline "Folder "
+            Write-Host -NoNewline  -ForegroundColor Green $FolderPermission.Identity
+            Write-Host -NoNewline " has "
+            Write-Host -NoNewline  -ForegroundColor Green $FolderPermission.AccessRights
+            Write-Host -NoNewline " permissions for user "
+            Write-Host -NoNewline  -ForegroundColor Green $FolderPermission.User
+            if ($Remove)
+            {
+                if ($TestMode)
+                {
+                    Write-Host -ForegroundColor Yellow " - Testing Removal"
+                    $FolderPermission | Remove-PublicFolderClientPermission -WhatIf
+                } else {
+                    Write-Host -ForegroundColor Red " - Removing"
+                    $FolderPermission | Remove-PublicFolderClientPermission -Confirm:$False
+                }
+            } else {
+                
+                Write-Host
+            }
+        }
+    }
+}
---
+++ Remove-PFPermissionsFromSSV.ps1
@@ -0,0 +1,25 @@
+﻿param($SSVOutput,$TestMode=$False)
+if (!(Test-Path -Path $SSVOutput))
+{
+    throw "File $($SSVOutput) not found"
+}
+[array]$SSVContent = Get-Content -Path $SSVOutput | Where {$_ -like "*permission needs to be removed*"}
+if (!$SSVContent)
+{
+    throw "No permissions found to remove"
+}
+foreach ($SSVLine in $SSVContent)
+{
+    $SSVLine=$SSVLine.Replace("this folder ","")
+    $SSVLine=$SSVLine.Replace("permission needs to be removed for user ","");
+    $Folder=($SSVLine.Split(" "))[0]
+    $SID="NT $(($SSVLine.Split(" "))[2])"
+    if ($TestMode)
+    {
+        Write-Host -ForegroundColor Green "Testing removing PF permission for $($Folder) for invalid SID $($SID)"
+        Get-PublicFolderClientPermission -Identity $Folder| Where {$_.User -like $SID} | Remove-PublicFolderClientPermission -WhatIf
+    } else {
+         Write-Host -ForegroundColor Green "Forcing removal of PF permission for $($Folder) for invalid SID $($SID)"
+        Get-PublicFolderClientPermission -Identity $Folder| Where {$_.User -like $SID} | Remove-PublicFolderClientPermission -Confirm:$False
+    }
+}
